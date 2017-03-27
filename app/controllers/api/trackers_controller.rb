1b7a2523f5d33d8c390ee5a3e2b12f98deadf3ff
Andreza Feu <andrezafeu@gmail.com>
2017-03-26 23:14:00 -0400

    Add show action for vehicle

--- app/controllers/api/trackers_controller.rb
+++ app/controllers/api/trackers_controller.rb
@@ -2,4 +2,9 @@ class Api::TrackersController < ApplicationController
   def index
     render json: Tracker.all
   end
+
+  def show
+    vehicle = Tracker.where(vehicle_id: params[:id])
+    render json: vehicle
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :trackers, only: [:index]
+    resources :trackers, only: [:index, :show]
   end
 end
